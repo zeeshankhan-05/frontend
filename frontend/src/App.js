aaf86dbd77347e27cae39447df4f99762b061875
zeeshankhan-05 <khanzeeshan1205@gmail.com>
2024-10-14 14:51:54 -0500

    Frontend development

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ClothesList from './components/ClothesList';
+import AddClothing from './components/AddClothing';
+import OutfitRecommendation from './components/OutfitRecommendation';
+import Preferences from './components/Preferences';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/add" component={AddClothing} />
+          <Route path="/closet" component={ClothesList} />
+          <Route path="/recommendations" component={OutfitRecommendation} />
+          <Route path="/preferences" component={Preferences} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
